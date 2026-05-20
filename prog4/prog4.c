0197506b9100e40e6d3e1507bf28e8e77b0a37aa
P YASHWANTH <pyatigoudruyashwanth@gmail.com>
2026-05-20 13:36:42 +0530

    completed program 4

--- prog4/prog4.c
+++ prog4/prog4.c
@@ -2,25 +2,22 @@
 #include <stdbool.h>
 
 struct Bitfield {
-    unsigned int firstbit : 1; // Allocates exactly 1 bit (values: 0 or 1)
-    unsigned int secondbit : 2; // Allocates 2 bits (values: 0 to 3)
-    unsigned int thirdbit : 5; // Allocates 5 bits (values: 0 to 31)
+    unsigned int firstbit : 1; 
+    unsigned int secondbit : 2; 
+    unsigned int thirdbit : 5; 
 };
 
 int main() {
-    // 2. Initialize the structure
+    
     struct Bitfield a;
-
-    // 3. Test assigning to the bitfield
     a.firstbit = 1;
     a.secondbit = 3;
     a.thirdbit = 17;
-
+    
     printf("firstbit: %u\n", a.firstbit);
     printf("status: %u\n", a.secondbit);
     printf("flags: %u\n", a.thirdbit);
     a.firstbit = 3;
     printf("firstbit after overflow (setting to 3): %u\n", a.firstbit);
-
     return 0;
 }
